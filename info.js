30d8c7d1725f7deb47afb57e4d551a1b98d86264
Ayaan Grover <ayaangrover@gmail.com>
2024-08-11 10:18:17 -0700

    code

---
+++ info.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIy9RJmcYCq1vEdZ1bmulL80YlqQa1duk",
+    authDomain: "hcc-site-2a199.firebaseapp.com",
+    projectId: "hcc-site-2a199",
+    storageBucket: "hcc-site-2a199.appspot.com",
+    messagingSenderId: "111351505999",
+    appId: "1:111351505999:web:4780ad4342212c6285b63a",
+    measurementId: "G-0LY0D15F68"
+};
